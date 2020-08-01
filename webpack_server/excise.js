77fd0b1f17d956089f236f0890deb56b9bea1c65
Le Toan <letoan@gmail.com>
2020-08-01 22:16:48 +0700

    bai tap

---
+++ webpack_server/excise.js
@@ -0,0 +1,16 @@
+
+
+
+// BT 1:  hien thi sanh sach post sau ra html URL = 'https://jsonplaceholder.typicode.com/posts';
+// Lưu ý có thể dùng hàm có sẵn của js là fetch hoặc cài đặt thêm axios
+// --------------------------------------------------------
+
+// BT 2: Từ url 'https://jsonplaceholder.typicode.com/posts/2', {2 la tham so id}
+// + viet 1 function voi tham so la URL va id, nguoi sau co the tai su dung
+// VD: getPost(url, id); khi truyền url và id vào sẽ nhận được response là 1 bài post 
+// ---------------------------------
+
+// BT 3:
+// + tìm hiểu localStorage,
+// + localStorage là async function hay sync function ?
+// + localStorage có những hàm hào, tác dụng của nó
